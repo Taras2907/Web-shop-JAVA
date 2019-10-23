5675e8ad172a6a943d53ca5bb2f2ca2988f69a11
Korneliusz Chodnikiewicz <korneliuszchodnikiewicz@gmail.com>
2019-10-23 14:24:50 +0200

    complete displaying products by category

--- src/main/java/com/codecool/shop/controller/CategoryController.java
+++ src/main/java/com/codecool/shop/controller/CategoryController.java
@@ -5,6 +5,7 @@ import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -15,28 +16,52 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(urlPatterns = {"/product/category"})
+@WebServlet(urlPatterns = {"/product"})
 public class CategoryController extends HttpServlet {
+    private ProductDao productDataStore = ProductDaoMem.getInstance();
+    private ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-
-        String productCategory = req.getParameter("category");
-
-
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
-        engine.process("product/index.html", context, resp.getWriter());
+
+        if (isValidRequestParameters(req)) {
+            setContext(context, req);
+            engine.process("product/index.html", context, resp.getWriter());
+        } else {
+            resp.sendRedirect(req.getContextPath());
+        }
+    }
+
+    private boolean isValidRequestParameters(HttpServletRequest req) {
+        String categoryId = req.getParameter("category");
+        if (categoryId != null && canConvertToInteger(categoryId)) {
+            ProductCategory productCategory = productCategoryDataStore.find(Integer.parseInt(categoryId));
+            List<Product> products = productDataStore.getBy(productCategory);
+            return products.size() > 0;
+        }
+        return false;
+    }
+
+    private boolean canConvertToInteger(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void setContext(WebContext context, HttpServletRequest req) {
+        String categoryId = req.getParameter("category");
+        ProductCategory productCategory = productCategoryDataStore.find(Integer.parseInt(categoryId));
+        List<Product> products = productDataStore.getBy(productCategory);
+        List<ProductCategory> categories = productCategoryDataStore.getAll();
+        context.setVariable("category", productCategory);
+        context.setVariable("products", products);
+        context.setVariable("categories", categories);
     }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -21,44 +21,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet(urlPatterns = {"/", "/product"})
+@WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
 
-        List<Product> products;
+        List<Product> products = productDataStore.getAll();
+        List<ProductCategory> categories = productCategoryDataStore.getAll();
+
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-
-        String categoryId = req.getParameter("category");
-
-        if (categoryId != null) {
-            try {
-                ProductCategory productCategory = productCategoryDataStore.find(Integer.parseInt(categoryId));
-            } catch (NumberFormatException e) {
-                resp.sendRedirect(req.getContextPath());
-                return;
-            }
-            ProductCategory productCategory = productCategoryDataStore.find(Integer.parseInt(categoryId));
-            products = productDataStore.getBy(productCategory);
-            if (products.size() > 0) {
-                context.setVariable("category", productCategory);
-                context.setVariable("products", products);
-                engine.process("product/filtered.html", context, resp.getWriter());
-            } else {
-                resp.sendRedirect(req.getContextPath());
-            }
-        } else {
-            products = productDataStore.getAll();
-            context.setVariable("products", products);
-            engine.process("product/index.html", context, resp.getWriter());
-        }
-
-
+        context.setVariable("products", products);
+        context.setVariable("categories", categories);
+        engine.process("product/index.html", context, resp.getWriter());
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
         // params.put("category", productCategoryDataStore.find(1));
