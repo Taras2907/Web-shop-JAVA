37d3398462ba462804831cc801c01d13edf6a4f5
Kendzioor <wojtekadam1@gmail.com>
2019-11-04 14:49:43 +0100

    Add side bar for supplier filter

--- src/main/java/com/codecool/shop/controller/CategoryController.java
+++ src/main/java/com/codecool/shop/controller/CategoryController.java
@@ -25,33 +25,38 @@ import java.util.List;
 public class CategoryController extends HttpServlet {
     private ProductDao productDataStore = ProductDaoMem.getInstance();
     private ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+    private SupplierDao supplierDaoMem = SupplierDaoMem.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
 
+
         if (isValidRequestParameters(req)) {
             setContext(context, req);
-            engine.process("product/index.html", context, resp.getWriter());
+            engine.process("product/test.html", context, resp.getWriter());
         } else {
             resp.sendRedirect(req.getContextPath());
         }
     }
 
     //TODO:
-    private boolean isValidRequestParameters(HttpServletRequest req, String parameter) {
-        String supplierId = req.getParameter(parameter);
+    private boolean isValidRequestParameters(HttpServletRequest req) {
+        String supplierId = req.getParameter("supplier");
         if (supplierId != null && canConvertToInteger(supplierId)){
-            Product supplierProducts = productDataStore.getBy()
+            Supplier supplier = supplierDaoMem.find(Integer.parseInt(supplierId));
+            List<Product> productsBySupplier = productDataStore.getBy(supplier);
+            return productsBySupplier.size() > 0;
         }
 
-        String categoryId = req.getParameter(parameter);
+        String categoryId = req.getParameter("category");
         if (categoryId != null && canConvertToInteger(categoryId)) {
             ProductCategory productCategory = productCategoryDataStore.find(Integer.parseInt(categoryId));
             List<Product> products = productDataStore.getBy(productCategory);
             return products.size() > 0;
         }
+
         return false;
     }
 
@@ -65,12 +70,32 @@ public class CategoryController extends HttpServlet {
     }
 
     private void setContext(WebContext context, HttpServletRequest req) {
-        String categoryId = req.getParameter("category");
-        ProductCategory productCategory = productCategoryDataStore.find(Integer.parseInt(categoryId));
-        List<Product> products = productDataStore.getBy(productCategory);
+
+        List<Product> products;
         List<ProductCategory> categories = productCategoryDataStore.getAll();
-        context.setVariable("category", productCategory);
-        context.setVariable("products", products);
+        List<Supplier> suppliers = supplierDaoMem.getAll();
+
+        //TODO: move the following validation into method
+        //It check which products must display and send to index
+
+        String supplierId = req.getParameter("supplier");
+        Supplier supplier;
+        if (supplierId != null){
+            supplier = supplierDaoMem.find(Integer.parseInt(supplierId));
+            products = supplier.getProducts();
+            context.setVariable("products", products);
+        }
+
+        String categoryId = req.getParameter("category");
+        ProductCategory productCategory;
+        if (categoryId != null){
+            productCategory = productCategoryDataStore.find(Integer.parseInt(categoryId));
+            context.setVariable("category", productCategory);
+            products = productDataStore.getBy(productCategory);
+            context.setVariable("products", products);
+        }
+
+        context.setVariable("suppliers", suppliers);
         context.setVariable("categories", categories);
     }
 }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -3,10 +3,13 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -24,15 +27,17 @@ public class ProductController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDaoMem = SupplierDaoMem.getInstance();
 
         List<Product> products = productDataStore.getAll();
         List<ProductCategory> categories = productCategoryDataStore.getAll();
-
+        List<Supplier> suppliers = supplierDaoMem.getAll();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("products", products);
         context.setVariable("categories", categories);
+        context.setVariable("suppliers", suppliers);
         engine.process("product/test.html", context, resp.getWriter());
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
