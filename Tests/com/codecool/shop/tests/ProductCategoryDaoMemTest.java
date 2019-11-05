9411a0557fe2dd39df8a8877dcd27c2ff959b9ed
Kendzioor <wojtekadam1@gmail.com>
2019-11-05 11:43:17 +0100

    Create first test

---
+++ Tests/com/codecool/shop/tests/ProductCategoryDaoMemTest.java
@@ -0,0 +1,12 @@
+package com.codecool.shop.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoMemTest {
+    @Test
+    void checkInstance(){
+
+    }
+}
