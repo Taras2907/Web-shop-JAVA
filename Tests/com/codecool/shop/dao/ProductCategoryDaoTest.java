e07c63dad51a196869b7ad0d23007803274a7e70
Kendzioor <wojtekadam1@gmail.com>
2019-11-06 13:06:18 +0100

    Creating tests

---
+++ Tests/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -0,0 +1,26 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductCategoryDaoTest {
+
+//    @Test
+//    public void checkAddedCategory(){
+//        ProductCategoryDaoMem productCategoryDao = ProductCategoryDaoMem.getInstance();
+//        ProductCategory productCategory = new ProductCategory(  "MobilePhone",
+//                                                                "Damn",
+//                                                                "Very good category");
+//        productCategoryDao.add(productCategory);
+//        String addedProductCategory = productCategoryDao.find(productCategoryDao.getAll().size()).toString();
+//        assertEquals("id: ", addedProductCategory);
+//    }
+
+    @Test
+    public void shit(){
+        System.out.println("shit");
+    }
+}
